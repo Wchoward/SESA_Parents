b00c0baff57fd6be0e4fc378de8b838f380abf52
Howard WU <Wchoward@users.noreply.github.com>
2020-05-08 15:17:37 +0800

    添加学生信息到班组中

--- pages/browse/index.js
+++ pages/browse/index.js
@@ -7,8 +7,8 @@ Page({
     data: {
         check_lst:[{
             usr_name:'李小绿',
-            create_time:'2020-05-07 03:21:25',
-            usr_avatar: '../../icons/coach.png',
+            create_time:'2020-05-07 20:21:25',
+            usr_avatar: '../../icons/lixiaolv.png',
             task_lst:[{
                 time:20,
                 content:'背单词'
@@ -16,9 +16,38 @@ Page({
                 time: 40,
                 content: '绘本阅读'
             }],
-            self_comment: '',
-            instruction: '',
-        },]
+            self_comment: '孩子今天读绘本了，好棒！',
+            instruction: '继续努力哦～～～',
+        }, 
+        {
+            usr_name: '邬小浩',
+            create_time: '2020-05-07 19:15:50',
+            usr_avatar: '../../icons/wuxiaohao.png',
+            task_lst: [{
+              time: 15,
+              content: '背单词'
+            }, {
+              time: 10,
+              content: '听音频'
+            }],
+            self_comment: '孩子觉得内容太难，学得少！',
+            instruction: '不要灰心，加油！',
+          },
+          {
+            usr_name: '张小红',
+            create_time: '2020-05-07 19:45:00',
+            usr_avatar: '../../icons/zhangxiaohong.png',
+            task_lst: [{
+              time: 25,
+              content: '背单词'
+            }, {
+              time: 60,
+              content: '影视观看'
+            }],
+            self_comment: '孩子很喜欢哈利波特，看了1个小时呢！',
+            instruction: '看的时候可以跟着一起读，锻炼口语哦～',
+          },
+        ]
     },
     
     /**
